6b3beac3820f30e1e1932efbdba1394225a8616c
Juni <yunior.varon@yahoo.com>
2021-02-15 23:05:49 +0100

    last commit

--- src/utils/login.js
+++ src/utils/login.js
@@ -3,18 +3,18 @@ const loginUrl = 'http://79.26.34.212:8000/login'
 // const body = { "username": "john", "password": "password123admin" }
 const refreshToken = 'http://79.26.34.212:8000/token'
 const userUrl = 'http://79.26.34.212:8000/users'
-const singleUserUrl = 'http://79.26.34.212:8000/users/'
+const singleUserUrl = 'http://79.26.34.212:8000/users'
 
-export const login = (username,password) => {
-  return axios.post(loginUrl,{username,password}).then(result => {
+export const login = (username, password) => {
+  return axios.post(loginUrl, { username, password }).then(result => {
     return result
   }).catch(err => console.log(err))
 }
 
 export const getUsers = (token) => {
-  return axios.get(userUrl,{
-    headers:{
-      Authorization: token ? `Bearer ${token} `: `Bearer ${localStorage.getItem('bearer')}`
+  return axios.get(userUrl, {
+    headers: {
+      Authorization: token ? `Bearer ${token} ` : `Bearer ${localStorage.getItem('bearer')}`
     }
   }).then(result => {
     console.log(result)
@@ -23,22 +23,24 @@ export const getUsers = (token) => {
 }
 
 export const refresh = () => {
-  return axios.post(refreshToken, {token: localStorage.getItem('refresh-bearer')}, {
-    headers: { 'Authorization' : `Bearer ${localStorage.getItem('bearer')}` }
-  }).then(result =>{
-     console.log(result)
-      return result
+  return axios.post(refreshToken, { token: localStorage.getItem('refresh-bearer') }, {
+    headers: { 'Authorization': `Bearer ${localStorage.getItem('bearer')}` }
+  }).then(result => {
+    console.log(result)
+    return result
   }).catch(err => console.log(err))
 }
 
-export const getUser = (id,username,password) => {
+export const getUser = (id, username, password) => {
   return axios.put(`${singleUserUrl}${id}`, { username, password }).then(result => {
     return result
   }).catch(err => console.log(err))
 }
 
-export const updateUser = (id,username, password) => {
-  return axios.put(`http://79.26.34.212:8000/users/${id}`, { username, password }).then(result => {
+export const updateUser = (id, username, password) => {
+  return axios.put(`${singleUserUrl}/${id}`, { username, password }, {
+    headers: { 'Authorization': `Bearer ${localStorage.getItem('bearer')}` }
+  }).then(result => {
     return result
   }).catch(err => console.log(err))
 }
